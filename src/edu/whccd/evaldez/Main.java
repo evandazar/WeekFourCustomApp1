800c085c5ba6a1f42572d97592d311498a668be5
evandazar <eval2380@giant.cos.edu>
2020-04-18 05:11:55 -0700

    Initial commit: Week 4 custom app on if/else statements and switch statements

---
+++ src/edu/whccd/evaldez/Main.java
@@ -0,0 +1,71 @@
+package edu.whccd.evaldez;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Numeric grade to a letter grade using if-else statements");
+	System.out.println("Enter a numeric integer grade (0-100):");
+	//System.out.println();
+
+	//Initiate variables
+    int numericGrade = 0;
+    char letterGrade;
+    String numericGradeRange = "";
+    Scanner sc = new Scanner(System.in);
+
+    //user inputs
+    numericGrade = sc.nextInt();
+    if (numericGrade < 0 || numericGrade > 100)
+        {
+        System.out.println("Invalid Input");
+        System.exit(0);
+        }
+    if (numericGrade >= 90)
+        {
+        letterGrade = 'A';
+        }
+    else if(numericGrade >= 80 && numericGrade < 90)
+        {
+        letterGrade = 'B';
+        }
+    else if(numericGrade >= 70 && numericGrade < 80)
+        {
+        letterGrade = 'C';
+        }
+    else if(numericGrade >= 60 && numericGrade < 70)
+        {
+        letterGrade = 'D';
+        }
+    else
+        {
+        letterGrade = 'F';
+        }
+    switch (letterGrade)
+    {
+        case 'A':
+            numericGradeRange = "90-100%";
+            break;
+        case 'B':
+            numericGradeRange = "80-89%";
+            break;
+        case 'C':
+            numericGradeRange = "70-79%;";
+            break;
+        case 'D':
+            numericGradeRange = "60-69%";
+            break;
+        default:
+            numericGradeRange = "0-59%";
+            break;
+    }
+    if (numericGradeRange != "")
+    {
+        String message = "Letter Grade: " + letterGrade + "\n" +
+                "***********************************************"
+                + "\n" + "Numeric Grade Range: " + numericGradeRange;
+        System.out.println(message);
+    }
+    }
+}
